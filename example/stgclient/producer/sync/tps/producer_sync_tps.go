bb0dd4dd246be221c2cfe43cae3270eeb640d767
tianyuliang <tianyuliang@139.com>
2017-10-19 20:30:18 +0800

    update 优化broker以及client启动example示例

--- example/stgclient/producer/sync/tps/producer_sync_tps.go
+++ example/stgclient/producer/sync/tps/producer_sync_tps.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strconv"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -12,49 +12,70 @@ import (
 	"git.oschina.net/cloudzone/smartgo/stgcommon/message"
 )
 
-func main() {
-	host := flag.String("h", "10.112.68.189:9876", "host")
-	topic := flag.String("t", "cloudzone1", "topic")
-	tag := flag.String("tag", "tagA", "tag")
-	producerGroupId := flag.String("pg", "producerGroupId-200", "producerGroupId")
+var (
+	def_producerGroupId = "producerGroupId-200" // 生产者组ID
+	def_topic           = "cloudzone123"        // topic
+	def_tag             = "tagA"                // tags
+	def_namesrvAddr     = "10.112.68.189:9876"  // namesrv地址
+	def_threadNum       = 200                   // 线程个数
+	def_everyThreadNum  = 50000                 // 每个线程发送消息次数
+	def_bodyDataSize    = 20                    // body数据长度
+)
 
-	goThreadNum := flag.Int("n", 1000, "thread num")
-	everyThreadNum := flag.Int("c", 5000, "thread/per send count")
-	//sendsize := flag.Int("s", 100, "send data size")
-	//async := flag.Bool("a", false, "sync & async")
+// ./tps -p producerGroupId-200 -topic cloudzone123 -tag tagA -h 10.112.68.189:9876 -n 200 -c 50000 -s 20
+// ./tps -n 200 -c 50000 -s 20
+func main() {
+
+	producerGroupId := flag.String("p", def_producerGroupId, "the producer group id ")
+	topic := flag.String("topic", def_topic, "the topic for use")
+	tag := flag.String("tag", def_tag, "the tag for use")
+	namesrvAddr := flag.String("h", def_namesrvAddr, "the namesrv ip:port")
+	gonum := flag.Int("n", def_threadNum, "go thread num")
+	sendnum := flag.Int("c", def_everyThreadNum, "one thread send msg count")
+	bodyDataSize := flag.Int("s", def_bodyDataSize, "send data size")
+
 	flag.Parse()
 
-	var wg sync.WaitGroup
+	var (
+		wg             sync.WaitGroup
+		goThreadNum    = *gonum
+		everyThreadNum = *sendnum
+		sucCount       int64
+		failCount      int64
+		start          int64
+		end            int64
+		tps            int64
+	)
 	defaultMQProducer := process.NewDefaultMQProducer(*producerGroupId)
-	defaultMQProducer.SetNamesrvAddr(*host)
+	defaultMQProducer.SetNamesrvAddr(*namesrvAddr)
 	defaultMQProducer.Start()
 
-	var success int64
-	var failed int64
-	total := (*goThreadNum) * (*everyThreadNum)
-	start := time.Now()
-	for i := 0; i < *goThreadNum; i++ {
+	start = time.Now().Unix()
+	for i := 0; i < goThreadNum; i++ {
 		wg.Add(1)
 		go func(n int) {
 			defer wg.Done()
-			for j := 0; j < *everyThreadNum; j++ {
-				sendResult, err := defaultMQProducer.Send(message.NewMessage(*topic, *tag, []byte("I'm so diao!呵呵"+strconv.Itoa(n)+"-"+strconv.Itoa(j))))
-				if err != nil {
-					atomic.AddInt64(&failed, 1)
-					fmt.Println("send msg err: ----> ", err.Error())
+			for j := 0; j < everyThreadNum; j++ {
+				// body := "I'm so diao!呵呵" + strconv.Itoa(n) + "-" + strconv.Itoa(j)
+				body := strings.Repeat("a", *bodyDataSize)
+				sendResult, err := defaultMQProducer.Send(message.NewMessage(*topic, *tag, []byte(body)))
+				if err != nil || sendResult == nil {
+					atomic.AddInt64(&failCount, 1)
+					if err != nil {
+						fmt.Println("send msg err: ----> ", err.Error())
+					}
 					continue
 				}
-				if sendResult != nil {
-					atomic.AddInt64(&success, 1)
-					//fmt.Println(sendResult.ToString())
-				}
+				atomic.AddInt64(&sucCount, 1)
+				//fmt.Println(sendResult.ToString())
 			}
 		}(i)
 	}
 
 	wg.Wait()
-	end := time.Now()
-	spend := end.Sub(start)
-	tps := success * 1000000000 / (end.UnixNano() - start.UnixNano())
-	fmt.Printf("Send Mssage. Time: %v, Total: %d, Thread: %d, EveryThreadNum: %d, Success: %d, Failed: %d, Tps: %d\n", spend, total, *goThreadNum, *everyThreadNum, success, failed, tps)
+	end = time.Now().Unix()
+	tps = sucCount / (end - start)
+
+	format := "msgCount=%d, goThreadNum=%d, everyThreadNum=%d, successCount=%d, failCount=%d, tps=%d \n"
+	fmt.Printf(format, goThreadNum*everyThreadNum, goThreadNum, everyThreadNum, sucCount, failCount, tps)
 }
